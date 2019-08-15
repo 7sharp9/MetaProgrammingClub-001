a85f4e54dcc3b8e9d509df22087d102ebf124b38
Dave Thomas <kukulcanenator@googlemail.com>
2019-08-15 19:05:26 +0100

    Add code

---
+++ csharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq.Expressions;
+//https://daveaglick.com/posts/using-expression-trees-to-get-class-and-member-names
+namespace csharp
+{
+    public class Foo
+    {
+        public int Bar { get; set; }
+        public int Baz(int red, string green) => 0;
+    }
+
+    class Program
+    {
+        public static (string, string) PropertyName<T>(Expression<Func<T, object>> expression)
+        {
+            var member = expression.Body as MemberExpression;
+            if (member == null)
+            {
+                member = (expression.Body as UnaryExpression)?.Operand as MemberExpression;
+            }
+            if (member == null)
+            {
+                throw new ArgumentException("Must be a member expression.");
+            }
+            return (typeof(T).Name, member.Member.Name);
+        }
+
+        public static (string, string) MemberName<T>(Expression<Func<T, string>> expression)
+        {
+            ConstantExpression constant = expression.Body as ConstantExpression;
+            if (constant == null)
+            {
+                throw new ArgumentException("Expression must be a constant expression.");
+            }
+            return (typeof(T).Name, constant.Value.ToString());
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PropertyName: {0}", PropertyName<Foo>(x => x.Bar));
+            Console.WriteLine("MemberName: {0}", MemberName<Foo>(x => nameof(x.Baz)));
+
+            //Evaluation
+            Expression<Func<int>> add = () => 1 + 2;
+            var func = add.Compile(); // Create Delegate
+            var answer = func(); // Invoke Delegate
+            Console.WriteLine("The answer is {0}", answer);
+        }
+    }
+}
---
+++ fsharp/Program.fs
@@ -0,0 +1,41 @@
+﻿open System
+open FSharp.Linq.RuntimeHelpers
+open FSharp.Quotations
+open FSharp.Quotations.Patterns
+open FSharp.Quotations.DerivedPatterns
+open Swensen.Unquote
+
+type TypeExtentions =
+    static member PropertyName([<ReflectedDefinition>] exp:Expr<_ -> _>) =
+        match exp with
+        | Lambda(arg, PropertyGet(_, propertyInfo, _)) ->
+            Some(propertyInfo.DeclaringType.Name, propertyInfo.Name)
+        | _ -> None
+
+    static member MethodName([<ReflectedDefinition>] exp:Expr<_ -> _>) =
+        match exp with
+        | Lambdas(_, Call(_, methodInfo,_)) ->
+            Some (methodInfo.DeclaringType.Name, methodInfo.Name)
+        | _ -> None
+
+type Foo =
+    {Bar: int}
+    member x.Baz(red, green) = 0
+
+[<EntryPoint>]
+let main argv =
+    printfn "PropertyName %A" (TypeExtentions.PropertyName (fun (x: Foo) -> x.Bar))
+    printfn "MethodName %A" (TypeExtentions.MethodName (fun (x: Foo) -> x.Baz))
+
+    //Evaluation
+    let add = <@ Func<unit, int>(fun () -> 1 + 2) @>
+    let func = LeafExpressionConverter.QuotationToLambdaExpression add
+    let compiled = func.Compile()
+    let answer = compiled.Invoke()
+    Console.WriteLine("The answer is {0}", answer)
+
+    //unquote
+    let answer = <@ fun () -> 1 + 2 @>.Eval()()
+    Console.WriteLine("The answer is {0}", answer)
+
+    0 // return an integer exit code
